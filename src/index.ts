0c9d83caf93fe71717329eccc6b1f83a83e18f7f
vcho1958 <gusckd57@gmail.com>
2023-01-09 15:56:35 +0900

    refactor change order public upper private

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import axios from "axios";
 
 
 class StationService {
+
+	public async printStationList():Promise<void> {
+		const res1 = await this.getStationsByPrice(1300)
+		const res2 = await this.getStationsByPrice(1400)
+		console.log(res1.concat(res2))
+	}
+
 	private async getStationsByPrice(price: number):Promise<string[]> {
 		try {
 			return await axios.get<[]>('https://test.oilnow.co.kr/gstations?price=' + price).then(res=>res.data)
@@ -10,11 +17,6 @@ class StationService {
 		}
 	}
 
-	public async printStationList():Promise<void> {
-		const res1 = await this.getStationsByPrice(1300)
-		const res2 = await this.getStationsByPrice(1400)
-		console.log(res1.concat(res2))
-	}
 }
 
 const stationService = new StationService()
