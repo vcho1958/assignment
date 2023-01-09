0c8adf2677245d6a553def7172e4507ba8491019
vcho1958 <gusckd57@gmail.com>
2023-01-09 15:55:25 +0900

    refctor change to class based logic

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,21 @@
 import axios from "axios";
 
-async function getStationsByPrice(price: number):Promise<string[]> {
-	try {
-		return await axios.get<[]>('https://test.oilnow.co.kr/gstations?price=' + price).then(res=>res.data)
-	} catch {
-		return []
+
+class StationService {
+	private async getStationsByPrice(price: number):Promise<string[]> {
+		try {
+			return await axios.get<[]>('https://test.oilnow.co.kr/gstations?price=' + price).then(res=>res.data)
+		} catch {
+			return []
+		}
+	}
+
+	public async printStationList():Promise<void> {
+		const res1 = await this.getStationsByPrice(1300)
+		const res2 = await this.getStationsByPrice(1400)
+		console.log(res1.concat(res2))
 	}
 }
 
-async function printStationList():Promise<void> {
-	const res1 = await getStationsByPrice(1300)
-	const res2 = await getStationsByPrice(1400)
-	console.log(res1.concat(res2))
-}
-
-printStationList()
+const stationService = new StationService()
+stationService.printStationList()
