59d810dcbc362f5236b2fdd548ee983be196458b
vcho1958 <gusckd57@gmail.com>
2023-01-09 15:58:22 +0900

    refactor: seperate class to file

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,5 @@
 import axios from "axios";
-
-
-class StationService {
-
-	public async printStationList():Promise<void> {
-		const res1 = await this.getStationsByPrice(1300)
-		const res2 = await this.getStationsByPrice(1400)
-		console.log(res1.concat(res2))
-	}
-
-	private async getStationsByPrice(price: number):Promise<string[]> {
-		try {
-			return await axios.get<[]>('https://test.oilnow.co.kr/gstations?price=' + price).then(res=>res.data)
-		} catch {
-			return []
-		}
-	}
-
-}
+import StationService from "./station.service";
 
 const stationService = new StationService()
 stationService.printStationList()
